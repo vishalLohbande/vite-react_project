da016cce8b92e52a32a6297f231bba265c5d744b
vishal_incubators <vishal@c2wincubators.in>
2025-07-21 10:31:19 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/vite-react_project.git/', // 👈 REPLACE with your GitHub repo name exactly
-})
